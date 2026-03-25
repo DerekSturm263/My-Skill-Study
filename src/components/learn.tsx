eae78b0094a98569bf7680bca4a60f537d5e18da
Derek <dereksturm263@gmail.com>
2026-03-25 07:40:44 -0700

    a

--- src/components/learn.tsx
+++ src/components/learn.tsx
@@ -177,6 +177,7 @@ export default function Page({ skill, id, mode }: { skill: Skill, id: string, mo
 
         {value.chapters.map((chapter, cIndex) => chapter.pages.map((page, pIndex) => (
           (cIndex == currentChapterIndex && pIndex == currentPageIndex && <PageComponent
+            key={`${cIndex},${pIndex}`}
             element={page}
             mode={mode}
             isThinking={isThinking}
