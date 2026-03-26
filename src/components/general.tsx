802ccf044d33c4d51a59daf9b1ce306a69182e0f
Derek <dereksturm263@gmail.com>
2026-03-26 11:35:40 -0700

    a

--- src/components/general.tsx
+++ src/components/general.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import 'globals.css';
+
 import ReorderList, { ReorderIcon } from 'react-reorder-list';
 import Files from '@/interactions/files/elements';
 import Drawing from '@/interactions/drawing/elements';
