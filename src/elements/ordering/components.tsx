b278224ca1ad2b11dafacdcb65685e96ee36d2d2
Derek <dereksturm263@gmail.com>
2026-03-27 18:18:41 -0700

    test ordering group

--- src/elements/ordering/components.tsx
+++ src/elements/ordering/components.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { Element, ElementPackage, ElementProps } from "@/lib/types/element";
-import { FormatLineSpacing } from '@mui/icons-material';
-import { useState } from 'react';
+import { DragHandle, FormatLineSpacing } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
 import { Type } from '@google/genai';
-import { Box } from '@mui/material';
+import { Box, FormControlLabel, Stack, Typography } from '@mui/material';
+import Markdown from "react-markdown";
+import ReorderList, { ReorderIcon } from "react-reorder-list";
 
 export interface ElementType extends Element {
   correctOrder: string[]
@@ -38,16 +40,41 @@ const schema = {
 };
 
 function Component(props: ElementProps<ElementType>) {
-  /*if (mode != types.ComponentMode.Edit) {
-    setItems(items.sort(item => Math.random() - 0.5));
-  }*/
+  useEffect(() => {
+    const newOrder = props.value.correctOrder.sort(item => Math.random() - 0.5);
+    props.setValue({ ... props.value, correctOrder: newOrder });
+  }, [])
 
   return (
     <Box
       sx={{ height: "100%", alignContent: 'center' }}
     >
-      
+      <ReorderList>
+        {props.value.correctOrder.map(item => (
+          <OrderingItem
+            item={item}
+          />
+        ))}
+      </ReorderList>
     </Box>
+  );
+}
+
+function OrderingItem({ item }: { item: string }) {
+  return (
+    <Stack
+      direction="row"
+    >
+      <ReorderIcon
+        draggable={true}
+      >
+        <DragHandle />
+      </ReorderIcon>
+
+      <Typography>
+        {item}
+      </Typography>
+    </Stack>
   );
 }
 
